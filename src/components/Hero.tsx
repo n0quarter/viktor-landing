d9e44a7833d88f35956c35b8fcd155ca0b772b58
Viktor Shcherban <megaletterbox@gmail.com>
2026-01-10 13:06:51 +0100

    make photo public

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
 import bgImage from "@/assets/bg8.png";
-import viktorPhoto from "@/assets/viktor.png";
 import { CONTACT_INFO } from "@/lib/constants";
 import { Check, Copy, Mail, MapPin } from "lucide-react";
 import { useState } from "react";
+// import viktorPhoto from "@/assets/viktor.png";
+const viktorPhoto = "/viktor.png";
 
 const Hero = () => {
   const [copiedEmail, setCopiedEmail] = useState(false);
@@ -39,13 +40,20 @@ const Hero = () => {
             <ul className="text-lg md:text-xl text-slate-300 mb-8 leading-relaxed space-y-2 list-disc list-inside">
 
                            <p className="text-lg md:text-xl text-slate-300 mb-8 leading-relaxed">
-              {/* Version 1:<br />  */}
+              Version 1:<br />
               18+ years of experience building software. Expert in designing text & voice AI Agents,
               RAG pipelines, and scalable architecture. Taking complex AI products from concept
               to production (0→1). Former CTO.
             </p>
 
-            {/* Version 2:
+            <p className="text-lg md:text-xl text-slate-300 mb-8 leading-relaxed">
+              Version 2:<br />
+              18+ years of experience building software. Expert in designing text & voice AI Agents,
+              RAG pipelines, and scalable architecture. Taking complex AI products from concept
+              to production (0→1). Former CTO.
+            </p>
+
+            Version 3:
               <li>
                 I build gen-AI products from concept to production (0→1)
                 <span className="block text-base text-slate-400 ml-8">
@@ -53,7 +61,7 @@ const Hero = () => {
                   </span>
               </li>
               <li>I've been building software for 18+ years</li>
-              <li>As a former startups' CTO, I specialize on fast yet robust MVPs</li> */}
+              <li>As a former startups' CTO, I specialize on fast yet robust MVPs</li>
             </ul>
 
             <div className="flex items-center gap-6 text-sm text-slate-300">
